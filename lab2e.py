90de5f1112f584c293178c806ac428a9f73e259e
An Truong <datruong@myseneca.ca>
2025-05-23 14:44:37 -0400

    Added all lab2 from a to g

---
+++ lab2e.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+#Author: datruong
+#Author ID: 166335232
+#Date Created: 2025/05/23
+
+timer = 10
+while timer != 0:
+    print(timer)
+    timer = timer - 1
+print("blast off!")
+
+
